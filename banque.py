1eb8cb27a18785e574c71fd903000a7b5b2d4ea1
albersonndaliko <albertwork40@gmail.com>
2024-06-02 19:43:38 +0200

    polymorphisme

--- banque.py
+++ banque.py
@@ -2,26 +2,26 @@ from abc import ABC, abstractmethod
 # Classe de Compte de base qui utilise l'encapsulation pour protéger les attributs
 class Compte(ABC):
     def __init__(self, titulaire, solde=0):
-        self._titulaire = titulaire  
+        self.__titulaire = titulaire  
 # Titulaire du compte, protégé par l'encapsulation
         self._solde = solde  
 # Solde du compte, protégé par l'encapsulation
 
 # Méthode pour afficher le solde du compte
     def afficher_solde(self):
-        return self._solde
+        return self.__solde
 
-  # Méthode pour effectuer un virement vers un autre compte 
+  # surcharge 
     def virementSurCompte(self, montant, autre_compte):
         if self.retirer(montant):
-            autre_compte.deposer(montant)
+            autre__compte.deposer(montant)
             return True
         return False
 
 # Méthode pour déposer de l'argent sur un compte
     def deposer(self, montant):
-        self._solde =self._solde + montant
-        return self._solde
+        self.__solde =self.__solde + montant
+        return self.__solde
 
 # Méthode abstraite pour retirer de l'argent, à définir dans les sous-classes
     @abstractmethod
@@ -41,8 +41,39 @@ class CompteEpargne(Compte):
             self._solde -= montant
             return "reussi avec succes"
         return "operation a echouer"
-    def deposer(self, montant):
-        print("deposez sur le compte principale")
 
+# polymorphisme
+    def deposerArgent(self,montant):
+        print("deposez dans le compte principale")
+
+
+# encapsulatiion
+class Compte(ABC):
+    def __init__(self, titulaire, solde=0):
+        self.__titulaire = titulaire  
+        self.__solde = solde 
+
+    def afficher_solde(self):
+        return self.__solde
+
+    def deposer(self, montant):
+        self.__solde =self.__solde + montant
+        return self.__solde
+# Utilisation de l'héritage, de la surcharge, du polymorphisme, de l'abstraction et de l'encapsulation
+compte1 = CompteCourant("Albert ", 1000)
+compte2 = CompteEpargne("ndaliko", 500)
+
+# albert dépose  sur son compte
+compte1.deposer(200)
+
+# ndaliko retire  sur son compte épargne
+compte2.retirer(100)
+
+# albert fait un virement à ndaliko
+compte1.virementSurCompte(300, compte2)
+
+# Affichage des soldes
+print(compte1.afficher_solde())  
+print(compte2.afficher_solde()) 
 
  
