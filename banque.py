0197ea34ade0de56c3214c0c2cf9c8e9e57c7224
albersonndaliko <albertwork40@gmail.com>
2024-06-02 18:28:18 +0200

    class heritage

--- banque.py
+++ banque.py
@@ -28,5 +28,19 @@ class Compte(ABC):
     def retirer(self, montant):
         pass
 
+# Classe CompteEpargne hérite de Compte et implémente la méthode retirer avec une vérification 
+class CompteEpargne(Compte):
+    def __init__(self, titulaire, solde=0):
+        super().__init__(titulaire, solde)  
+# Appel du constructeur de la classe parent
+
+# Surcharge de la méthode retirer pour le CompteEpargne avec une vérification de solde minimum
+    def retirer(self, montant):
+        if montant <= self._solde and self._solde - montant >= 0:  
+# Solde minimum de 50 après retrait
+            self._solde -= montant
+            return "reussi avec succes"
+        return "operation a echouer"
+
 
  
